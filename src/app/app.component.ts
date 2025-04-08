58594afe323fa2698a3651073f0c339ae131a5a0
LanaNew <l4n.steen@gmail.com>
2025-04-08 20:03:08 +0400

    ang 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,28 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   
 
-  sayHello ="Hello World!"
+  sayHello =""
+  imgSorce = "https://images.unsplash.com/photo-1726137569906-14f8079861fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  imgClass =""
+  imgStyle = "border-radius: 10px"
+
+
+  user = {
+    name : "John",
+    lastName : "Doe"
+  }
+
+  print(){
+    console.log("hi")
+    this.chnageImg()
+    this.sayHello == "" ?  this.sayHello ="hello user" :  this.sayHello =""
+  }
+
+
+  chnageImg(){
+  this.imgClass = "mainImg"
+  }
+
 
  
 
