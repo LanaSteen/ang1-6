993afbce980bed6d1cb34c962807255a2487c01a
LanaNew <l4n.steen@gmail.com>
2025-04-26 20:04:14 +0400

    ang 6

--- src/app/Models/User.ts
+++ src/app/Models/User.ts
@@ -1,8 +1,7 @@
-
-export class User{
-    name? : string;
-    lastName? : string;
-    email? : string;
-    
-
+export class User {
+    id!: number;
+    email!: string;
+    first_name!: string;
+    last_name!: string
+    avatar!: string
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
 import { StudentComponent } from './student/student.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'main', component: MainComponent},
     {path: 'student', component: StudentComponent},
+    {path: "users", component: UsersComponent},
+    {path: "users/:id", component: UserDetailsComponent}
 ];
